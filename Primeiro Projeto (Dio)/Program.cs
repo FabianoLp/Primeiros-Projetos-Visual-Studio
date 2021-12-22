4167f5654db92b9b199fa101edee7cf247b20226
FabianoLp <91691851+FabianoLp@users.noreply.github.com>
2021-12-22 02:45:15 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro Projeto (Dio)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] AssemblyLoadEventArgs)
+         {
+           Console.WriteLine("Hello World");   
+         }
+    }
+ }
